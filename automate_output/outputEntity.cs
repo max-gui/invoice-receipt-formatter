6dba65ed3e09075361ba0dd975b828b5be8f53eb
jimmy katsura <playstation.gui@gmail.com>
2016-04-09 14:48:55 +0800

    real init

---
+++ automate_output/outputEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automate_output
+{
+    class outputEntity
+    {
+
+        public string NameIndex1 { get; set; }
+        public string Data2 { get; set; }
+        public string OnTime3 { get; set; }
+        public string DownTime4 { get; set; }
+        public string Money5 { get; set; }
+        public string WaitingTime6 { get; set; }
+        public string Message7 { get; set; }
+
+        //public string Name { get; set; }
+        //public string Number { get; set; }
+        //public string Employee_ID { get; set; }
+        //public string Email_ID { get; set; }
+    }
+}
